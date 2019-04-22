1884b1200df02b356f8065648ec49b18c0c00501
skandalabrandur <steinerinn@gmail.com>
2019-04-22 15:45:55 +0000

    Animations enabled for custom scrolling

--- site.js
+++ site.js
@@ -5,10 +5,17 @@ var makeItGo = (loc) => {
 
 }
 
-$("document").ready(
+
+$(document).ready(
     () => {
         $(".sidebar div a").on("click", (e) => {
             makeItGo(e.target.dataset.loc);
         });
     }
 );
+
+$(document).on('scroll', () => {
+    if($(this).scrollTop() >= $('#projects-block').position().top){
+        $(".sub-level-nav").css("display", "flex").animate({marginLeft: "0px"});
+    }
+})
