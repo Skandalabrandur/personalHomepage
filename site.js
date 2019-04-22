5e555b661a168264a77460368023d48c9ac3c262
skandalabrandur <steinerinn@gmail.com>
2019-04-22 17:33:57 +0000

    Skillset scaffold

--- site.js
+++ site.js
@@ -11,11 +11,18 @@ $(document).ready(
         $(".sidebar div a").on("click", (e) => {
             makeItGo(e.target.dataset.loc);
         });
+
+        $(".skill-child").each(function() {
+            $(this).children().css("width", $(this).data("prog") + "%");
+        });
+
     }
+
 );
 
 $(document).on('scroll', () => {
-    if($(this).scrollTop() >= $('#projects-block').position().top){
+    if($(this).scrollTop() + 10 >= $('#projects-block').position().top){
         $(".sub-level-nav").css("display", "flex").animate({marginLeft: "0px"});
     }
 })
+
